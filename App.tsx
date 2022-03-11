9131cb344406b0e94dfd7d7693406d01d9251d31
Enio Gabriel <enio@nexti.com>
2022-03-11 11:42:27 -0300

    criação do projeto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screen/Home';
 
 export default function App() {
   return (
-    <Text>
-      ${`hello world!\n\n\n\n\n
-      aqui`}
-    </Text>
+    <Home />
   );
 }
